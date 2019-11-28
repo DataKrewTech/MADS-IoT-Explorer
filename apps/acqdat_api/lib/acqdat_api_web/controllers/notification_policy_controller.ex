1b7eb39277f898007ee542cc4e5244c33b364cef
Vikram Singh Jadon <vikramjadon921995@gmail.com>
2019-11-28 10:25:55 +0530

    Modified policy listing API to use correct views

--- apps/acqdat_api/lib/acqdat_api_web/controllers/notification_policy_controller.ex
+++ apps/acqdat_api/lib/acqdat_api_web/controllers/notification_policy_controller.ex
@@ -1,16 +1,12 @@
 defmodule AcqdatApiWeb.NotificationPolicyController do
   use AcqdatApiWeb, :controller
   alias AcqdatCore.Model.SensorNotification, as: SensorNotificationModel
-  import AcqdatApiWeb.Helpers
 
   def index(conn, _params) do
-    with {:list, [policies]} = {:list, SensorNotificationModel.get_policies_with_preferences()} do
+    with {:list, policies} = {:list, SensorNotificationModel.get_policies_with_preferences()} do
       conn
       |> put_status(200)
-      |> render("policy.json", policies: policies)
-    else
-      {:extract, {:error, error}} ->
-        send_error(conn, 400, error)
+      |> render("policies.json", policies: policies)
     end
   end
 end
--- apps/acqdat_api/lib/acqdat_api_web/views/notification_policy_view.ex
+++ apps/acqdat_api/lib/acqdat_api_web/views/notification_policy_view.ex
@@ -2,18 +2,18 @@ defmodule AcqdatApiWeb.NotificationPolicyView do
   use AcqdatApiWeb, :view
   alias AcqdatApiWeb.NotificationPolicyView
 
-  def render("policies.json", %{notification_policy: {:policies, policies}}) do
+  def render("policy.json", %{notification_policy: policy}) do
     %{
-      policy_name: policies.policy_name,
-      preferences_name: policies.preferences.name,
-      rule_data: policies.preferences.rule_data,
-      rule_name: policies.rule_name
+      policy_name: policy.policy_name,
+      preferences_name: policy.preferences.name,
+      rule_data: policy.preferences.rule_data,
+      rule_name: policy.rule_name
     }
   end
 
-  def render("policy.json", %{policies: policies}) do
+  def render("policies.json", %{policies: policies}) do
     %{
-      policy_list: render_many(%{policies: policies}, NotificationPolicyView, "policies.json")
+      policies: render_many(policies, NotificationPolicyView, "policy.json")
     }
   end
 end
