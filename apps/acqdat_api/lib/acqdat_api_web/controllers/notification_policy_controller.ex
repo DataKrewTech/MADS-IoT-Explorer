9fa66e557da4323f1653252cce3820d4ecc8acf1
Vikram Singh Jadon <vikramjadon921995@gmail.com>
2019-12-07 17:54:00 +0530

    Removed syntax of with from the index API of notification policy controller

--- apps/acqdat_api/lib/acqdat_api_web/controllers/notification_policy_controller.ex
+++ apps/acqdat_api/lib/acqdat_api_web/controllers/notification_policy_controller.ex
@@ -3,10 +3,9 @@ defmodule AcqdatApiWeb.NotificationPolicyController do
   alias AcqdatCore.Model.SensorNotification, as: SensorNotificationModel
 
   def index(conn, _params) do
-    with {:list, policies} = {:list, SensorNotificationModel.get_policies_with_preferences()} do
-      conn
-      |> put_status(200)
-      |> render("policies.json", policies: policies)
-    end
+    {:list, policies} = {:list, SensorNotificationModel.get_policies_with_preferences()}
+    conn
+    |> put_status(200)
+    |> render("policies.json", policies: policies)
   end
 end
