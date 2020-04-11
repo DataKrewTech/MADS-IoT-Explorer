89cfea60f16ce6ebd0509173d34d0b11003ce496
BandanaPandey <bandanapandey11@gmail.com>
2020-04-12 01:08:36 +0530

    Moved the User Show api, fetch from UserModel to user_context

--- apps/acqdat_api/lib/acqdat_api_web/controllers/user_controller.ex
+++ apps/acqdat_api/lib/acqdat_api_web/controllers/user_controller.ex
@@ -1,35 +1,25 @@
 defmodule AcqdatApiWeb.UserController do
   use AcqdatApiWeb, :controller
-  alias AcqdatCore.Model.User, as: UserModel
+  alias AcqdatApi.User
   import AcqdatApiWeb.Helpers
 
-  plug :load_user when action in [:show]
-
-  def show(conn, _params) do
+  def show(conn, %{"id" => id}) do
     case conn.status do
       nil ->
-        user = conn.assigns.user
+        {id, _} = Integer.parse(id)
 
-        conn
-        |> put_status(200)
-        |> render("user_details.json", %{user_details: user})
+        with {:show, {:ok, user}} <- {:show, User.get(id)} do
+          conn
+          |> put_status(200)
+          |> render("user_details.json", %{user_details: user})
+        else
+          {:show, {:error, message}} ->
+            send_error(conn, 400, message)
+        end
 
       404 ->
         conn
         |> send_error(404, "Resource Not Found")
-    end
-  end
-
-  defp load_user(%{params: %{"id" => user_id}} = conn, _params) do
-    {user_id, _} = Integer.parse(user_id)
-
-    case UserModel.get(user_id) do
-      {:ok, user} ->
-        assign(conn, :user, user)
-
-      {:error, _message} ->
-        conn
-        |> put_status(404)
     end
   end
 end
