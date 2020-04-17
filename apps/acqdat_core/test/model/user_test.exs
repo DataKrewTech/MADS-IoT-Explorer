2747239d5cc6967a13d4b53e8d67ff6b321c1a64
BandanaPandey <bandanapandey11@gmail.com>
2020-04-17 22:05:04 +0530

    Fixed the code format

--- apps/acqdat_core/test/model/user_test.exs
+++ apps/acqdat_core/test/model/user_test.exs
@@ -6,11 +6,11 @@ defmodule AcqdatCore.Model.UserTest do
   alias AcqdatCore.Model.User
 
   describe "set_asset/2" do
-  	test "associates assets with user" do
+    test "associates assets with user" do
       user = insert(:user)
       asset = insert(:asset)
 
-      asset_params =  [%{"id" => asset.id, "name" => asset.name}]
+      asset_params = [%{"id" => asset.id, "name" => asset.name}]
 
       {:ok, result} = User.set_asset(user, asset_params)
       assert not is_nil(result)
@@ -18,11 +18,11 @@ defmodule AcqdatCore.Model.UserTest do
   end
 
   describe "set_app/2" do
-  	test "associates app with user" do
+    test "associates app with user" do
       user = insert(:user)
       app = insert(:app)
 
-      asset_params =  [%{"id" => app.id, "name" => app.name}]
+      asset_params = [%{"id" => app.id, "name" => app.name}]
 
       {:ok, result} = User.set_apps(user, asset_params)
       assert not is_nil(result)
