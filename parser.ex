202d3552f2e5e9a5c33562714ee57adf6fb6450c
BandanaPandey <bandanapandey11@gmail.com>
2020-05-08 10:20:36 +0530

    Parser Changes

--- parser.ex
+++ parser.ex
@@ -1,14 +1,16 @@
 defmodule EntityParser do
   alias AcqdatCore.Model.Sensor, as: SensorModel
   alias AcqdatCore.Model.Asset, as: AssetModel
+  alias AcqdatCore.Model.Organisation, as: OrgModel
   import AsNestedSet.Queriable, only: [dump_one: 2]
   
-  def parse(entities, org_id, parent_id, parent_type) do
+  #EntityParser.parse(k["entities"], k["id"], nil, k["type"], nil)
+  def parse(entities, org_id, parent_id, parent_type, parent_entity) do
     if entities !== nil do
       for entity <- entities do
-          parent_entity = case {entity["type"], entity["action"]} do
+          result = case {entity["type"], entity["action"]} do
             {"Asset", "create"} ->
-              asset_creation(entity, org_id, parent_id, parent_type)
+              asset_creation(entity, org_id, parent_id, parent_type, parent_entity)
             {"Asset", "update"} ->
               asset_updation(entity, org_id)
             {"Asset", "delete"} ->
@@ -16,7 +18,7 @@ defmodule EntityParser do
             {"Asset", nil} ->
               nil
             {"Sensor", "create"} ->
-              sensor_creation(entity, org_id, parent_id, parent_type)
+              sensor_creation(entity, org_id, parent_id, parent_type, parent_entity)
             {"Sensor", "update"}  ->
               sensor_updation(entity, org_id)
             {"Sensor", "delete"} ->
@@ -24,63 +26,77 @@ defmodule EntityParser do
             {"Sensor", nil} ->
               nil
             {"Parameter", _} ->
-              nil          end
-          parse(entity["entities"], org_id, entity["id"], entity["type"])
+              nil 
+            end
+
+            case result do
+              {:ok, parent_entity} ->
+                parse(entity["entities"], org_id, entity["id"], entity["type"], parent_entity)
+              nil ->
+                parse(entity["entities"], org_id, entity["id"], entity["type"], nil)
+            end
       end
     end
   end
 
-  defp asset_creation(entity, org_id, parent_id, parent_type) do
-    IO.puts("inside asset_creation")
-    parent_asset = case {parent_type, parent_id} do
-      {"Organisation", nil} ->
+  defp asset_creation(entity, org_id, parent_id, parent_type, parent_entity) do
+    parent_asset = case {parent_type, parent_id, parent_entity} do
+      {"Organisation", nil, _} ->
         add_asset_as_root(entity, org_id)
-      {"Asset", _} ->
+      {"Asset", _, nil} ->
         add_asset_as_child(entity, org_id, parent_id)
+      {"Asset", _, _} ->
+        add_asset_as_child(entity, org_id, parent_entity.id)
     end
     parent_asset
   end
 
   defp add_asset_as_root(%{"name" => name}, org_id) do
-    AssetModel.add_as_root(%{name: name, org_id: org_id})
+    {:ok, org} = OrgModel.get_by_id(org_id)
+    AssetModel.add_as_root(%{name: name, org_id: org_id, org_name: org.name})
   end
-
-  # defp add_asset_as_child(%{"name" => name}, org_id, parent_id) do
-  #   AssetModel.add_asset_as_child(parent_entity, name, org_id, :child)
-  # end
 
   defp add_asset_as_child(%{"name" => name}, org_id, parent_id) do
     {:ok, parent_entity} = AssetModel.get(parent_id)
-    #AcqdatCore.Model.Asset.add_asset_as_child(nil, "Common Space 34535", 1, :child)
     AssetModel.add_as_child(parent_entity, name, org_id, :child)
   end
 
-  defp asset_updation(entity, org_id) do
-    IO.puts("inside asset_updation")
+  defp asset_updation(%{"id" => id, "name" => name}, org_id) do
+    IO.puts("inside asset asset_updation")
+    {:ok, asset} = AssetModel.get(id)
+    AssetModel.update(asset, %{
+      name: name
+    })
     nil
   end
 
   defp asset_deletion(%{"id" => id}) do
-    IO.puts("inside asset_deletion")
     {:ok, asset} = AssetModel.get(id)
     AssetModel.delete(asset)
     nil
   end
 
-  defp sensor_creation(%{"name" => name}, org_id, parent_id, parent_type) do
-    IO.puts("inside sensor_creation")
-    IO.puts(parent_id)
-    SensorModel.create(%{
-      name: name,
-      parent_id: parent_id,
-      parent_type: parent_type,
-      org_id: org_id
-    })
+  defp sensor_creation(%{"name" => name}, org_id, parent_id, parent_type, parent_entity) do
+    case parent_entity do
+      nil ->
+        SensorModel.create(%{
+          name: name,
+          parent_id: parent_id,
+          parent_type: parent_type,
+          org_id: org_id
+        })
+      _ ->
+        SensorModel.create(%{
+          name: name,
+          parent_id: parent_entity.id,
+          parent_type: "Asset",
+          org_id: org_id
+        })
+    end
     nil
   end
 
   defp sensor_updation(%{"id" => id, "name" => name, "parent_id" => parent_id, "parent_type" => parent_type}, org_id) do
-    IO.puts("inside sensor_updation")
     {:ok, sensor} = SensorModel.get(id)
     SensorModel.update(sensor, %{
       name: name,
@@ -91,7 +107,6 @@ defmodule EntityParser do
   end
 
   defp sensor_deletion(%{"id" => id}) do
-    IO.puts("inside sensor_deletion")
     SensorModel.delete(id)
     nil
   end
