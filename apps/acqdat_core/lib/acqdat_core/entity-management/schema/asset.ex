8b7ea14325fa8333f67c54b6bea47154978d6301
Ayoush <ayoush@Ayoushs-MacBook-Air.local>
2020-05-11 18:56:22 +0530

    asset create api addition in the codebase

--- apps/acqdat_core/lib/acqdat_core/entity-management/schema/asset.ex
+++ apps/acqdat_core/lib/acqdat_core/entity-management/schema/asset.ex
@@ -66,6 +66,7 @@ defmodule AcqdatCore.Schema.EntityManagement.Asset do
     field(:image_url, :string)
     field(:image, :any, virtual: true)
 
+
     # associations
     belongs_to(:org, Organisation, on_replace: :delete)
     belongs_to(:project, Project, on_replace: :delete)
