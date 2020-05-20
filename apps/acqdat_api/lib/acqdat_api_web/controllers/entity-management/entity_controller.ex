4704593946d6196682ec53c54bc08d7b4acc4879
BandanaPandey <bandanapandey11@gmail.com>
2020-05-20 13:03:29 +0530

    Formatted code

--- apps/acqdat_api/lib/acqdat_api_web/controllers/entity-management/entity_controller.ex
+++ apps/acqdat_api/lib/acqdat_api_web/controllers/entity-management/entity_controller.ex
@@ -37,6 +37,7 @@ defmodule AcqdatApiWeb.EntityManagement.EntityController do
     case conn.status do
       nil ->
         org = conn.assigns.org
+
         conn
         |> put_status(200)
         |> render("organisation_tree.json", org)
