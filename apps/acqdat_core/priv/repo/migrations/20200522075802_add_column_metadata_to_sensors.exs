1a6c7114151af8a39b438f7ac20b2de8ada3b63c
BandanaPandey <bandanapandey11@gmail.com>
2020-06-05 11:56:52 +0530

    removed duplicate migrations regarding addition of metadata column to sensors table

--- apps/acqdat_core/priv/repo/migrations/20200522075802_add_column_metadata_to_sensors.exs
+++
@@ -1,9 +0,0 @@
-defmodule AcqdatCore.Repo.Migrations.AddColumnMetadataToSensors do
-  use Ecto.Migration
-
-  def change do
-    alter table("acqdat_sensors") do
-      add(:metadata, {:array, :map})
-    end
-  end
-end
