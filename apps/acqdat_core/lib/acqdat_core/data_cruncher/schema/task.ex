c80d0d51376978567c0554f3a14151b05399bbb0
arjun289 <arjun289singh@gmail.com>
2020-06-05 11:32:32 +0800

    Modify Task schema to use embeds for workflows
    Earlier graph and data were separated columns, however
    a task should consists of multiple workflows, and the workflow
    graph as well as it's initialization data should be clubbed
    together.

--- apps/acqdat_core/lib/acqdat_core/data_cruncher/schema/task.ex
+++ apps/acqdat_core/lib/acqdat_core/data_cruncher/schema/task.ex
@@ -9,37 +9,49 @@ defmodule AcqdatCore.DataCruncher.Schema.Tasks do
   A user can create different tasks to perform different types of operations on
   raw or processed data.
 
-  A task consists of multiple graphs. Data Cruncher app makes use of Flow Based
-  Programming to create data processing pipelines. A flow or a graph is represented
-  using a directed acyclic graph of components connected by edges.
+  A task consists of multiple workflows/graphs. Data Cruncher app makes use of Flow Based
+  Programming to create data processing pipelines. A workflow/graph is represented
+  using a directed acyclic graph of `components` connected by edges.
+  See `Virta.Component`.
   """
   use AcqdatCore.Schema
   alias AcqdatCore.Schema.EntityManagement.Organisation
   alias AcqdatCore.Schema.RoleManagement.User
+  alias AcqdatCore.DataCruncher.Schema.Workflow
 
   @task_types ~w(one_time periodic triggered)a
 
   @typedoc """
-  `graphs`: The graphs which defines the flow of data and functions in the task.
-  `data`: The intitial data which is passed to the graph to inititate execution.
+  `workflows`: Workflows which are present in the task. See `Workflow`.
   `task_type`: Defines what kind of a task it is, it can be one of `one_time`,
               `periodic`, or `triggered`.
   """
   @type t :: %__MODULE__{}
   schema("acqdat_tasks") do
-    field(:graphs, {:array, :map})
-    field(:data, :map)
     field(:type, :string)
-
     field(:uuid, :string, null: false)
     field(:slug, :string, null: false)
 
     belongs_to(:org, Organisation, on_replace: :delete)
     belongs_to(:user, User, on_replace: :raise)
+
+    embeds_many(:workflows, Workflow, on_replace: :delete)
+
+    timestamps(type: :utc_datetime)
   end
 
   @required ~w(graphs data type org_id user_id)a
 
+  @doc """
+  Returns a changeset for performing `create` and `update` operations.
+
+  **Note**
+  Workflows set inside a task are set as embedded params, please make sure
+  to pass in the entire list of workflows during `update` operation else
+  it would be removed from the record.
+  See `Ecto.Changeset.cast_embed(changeset, name, opts \\ [])` and
+  `Ecto.Changeset.cast_assoc(changeset, name, opts \\ [])`
+  """
   def changeset(%__MODULE__{} = task, params) do
     task
     |> cast(params, @required)
@@ -48,5 +60,30 @@ defmodule AcqdatCore.DataCruncher.Schema.Tasks do
     |> validate_inclusion(:type, @task_types)
     |> add_slug()
     |> add_uuid()
+    |> cast_embed(:workflows, with: &Workflow.changeset/2)
   end
+
+end
+
+defmodule AcqdatCore.DataCruncher.Schema.Workflow do
+    @moduledoc """
+    Models a workflow.
+
+    A workflow schema consists of a graph and data with which nodes/vertices in the
+    graph are intitialized with.
+    """
+
+    use AcqdatCore.Schema
+
+    @type t :: %__MODULE__{}
+
+    embedded_schema do
+      field(:graph, :map)
+      field(:data, {:array, :map})
+    end
+
+    def changeset(%__MODULE__{} = workflow, params) do
+      workflow
+      |> cast(params, [:graph, :data])
+    end
 end
