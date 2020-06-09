1a4ecde1ca2eb90080018962ec195f57003201e8
BandanaPandey <bandanapandey11@gmail.com>
2020-06-09 12:22:37 +0530

    Fixed the testcases

--- apps/acqdat_api/lib/acqdat_api_web/views/entity-management/asset_view.ex
+++ apps/acqdat_api/lib/acqdat_api_web/views/entity-management/asset_view.ex
@@ -58,8 +58,7 @@ defmodule AcqdatApiWeb.EntityManagement.AssetView do
       parent_id: asset.parent_id,
       asset_type_id: asset.asset_type_id,
       creator_id: asset.creator_id,
-      metadata: render_many(asset.metadata, AssetView, "metadata.json"),
-      mapped_parameters: render_many(asset.mapped_parameters, AssetView, "parameters.json")
+      metadata: render_many(asset.metadata, AssetView, "metadata.json")
     }
   end
 
