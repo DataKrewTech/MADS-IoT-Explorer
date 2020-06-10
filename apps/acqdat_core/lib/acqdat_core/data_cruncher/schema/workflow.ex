b45c87951182588756a7e42fb6132b6f5a6fbb83
BandanaPandey <bandanapandey11@gmail.com>
2020-06-10 17:45:53 +0530

    Added new table for workflows
    - Each task can have multiple workflows, through has_may associations
    - Each workflow consists of following things:
     - graph
     - input_data
     - uuid

--- apps/acqdat_core/lib/acqdat_core/data_cruncher/schema/workflow.ex
+++ apps/acqdat_core/lib/acqdat_core/data_cruncher/schema/workflow.ex
@@ -1,6 +1,6 @@
 defmodule AcqdatCore.DataCruncher.Schema.Workflow do
   @moduledoc """
-  Models a workflow temporary output.
+  Models a workflow.
 
   A workflow schema consists of a graph and data with which nodes/vertices in the
   graph are intitialized.
---
+++ apps/acqdat_core/test/data_cruncher/domain/workflow.ex
@@ -0,0 +1,9 @@
+defmodule AcqdatCore.DataCruncher.Domain.WorkflowTest do
+  use ExUnit.Case, async: true
+  use AcqdatCore.DataCase
+  import AcqdatCore.Factory
+  alias AcqdatCore.Test.Support.SensorsData
+
+  describe "register/1" do
+  end
+end
