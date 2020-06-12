093e32c6f40a1d73ec68d6e9023c14a0ba838c1f
BandanaPandey <bandanapandey11@gmail.com>
2020-06-12 21:05:23 +0530

    Fixed seed files: for assets and sensors

--- apps/acqdat_core/priv/repo/seed/entity-management/asset.ex
+++ apps/acqdat_core/priv/repo/seed/entity-management/asset.ex
@@ -3,8 +3,11 @@ defmodule AcqdatCore.Seed.EntityManagement.Asset do
   alias AcqdatCore.Schema.RoleManagement.User
   alias AcqdatApiWeb.Helpers
   import AsNestedSet.Modifiable
-  alias AcqdatCore.Repo
   import Tirexs.HTTP
+  alias AcqdatCore.Schema.EntityManagement.{Asset, Organisation, Project, AssetType}
+  alias AcqdatCore.Schema.RoleManagement.User
+  alias AcqdatApiWeb.Helpers
+  alias AcqdatCore.Repo
 
   @asset_manifest [
     {
@@ -37,6 +40,7 @@ defmodule AcqdatCore.Seed.EntityManagement.Asset do
 
   def create_taxonomy({parent, children, properties}, asset_type, org, project) do
     [user] = Repo.all(User)
+
     asset =
       Repo.preload(
         %Asset{
@@ -54,7 +58,7 @@ defmodule AcqdatCore.Seed.EntityManagement.Asset do
         :org
       )
 
-     root = add_root(asset)
+      root = add_root(asset)
      insert_asset("assets", root)
      for taxon <- children do
        create_taxon(taxon, root, asset_type, user)
@@ -84,9 +88,7 @@ defmodule AcqdatCore.Seed.EntityManagement.Asset do
           creator_id: user.id
           },
           [:org])
-
     {:ok, root} = add_taxon(root, child, :child)
-
     for taxon <- children do
       create_taxon(taxon, root, asset_type, user)
     end
@@ -136,3 +138,19 @@ defmodule AcqdatCore.Seed.EntityManagement.Asset do
   end
 end
 
+  defp seed_asset_type(org, project) do
+    asset_type = %AssetType{
+        name: "Asset Type",
+        description: "This is seeded asset type",
+        sensor_type_present: false,
+        uuid: UUID.uuid1(:hex),
+        slug: Slugger.slugify("Asset Type"),
+        org_id: org.id,
+        project_id: project.id
+     }
+     case Repo.insert(asset_type) do
+       {:ok, asset_type} -> asset_type
+       {:error, error} -> raise RuntimeError, message: "Problem Inserting Asset Type"
+     end
+  end
+end
