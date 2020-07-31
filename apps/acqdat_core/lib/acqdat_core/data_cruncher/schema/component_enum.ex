85359d29ad5bfbe9383d7bbcc00df0892cc8d5a5
BandanaPandey <bandanapandey11@gmail.com>
2020-07-31 16:26:58 +0530

    Added following functions as components: -> mean -> variance -> standard_deviation

--- apps/acqdat_core/lib/acqdat_core/data_cruncher/schema/component_enum.ex
+++ apps/acqdat_core/lib/acqdat_core/data_cruncher/schema/component_enum.ex
@@ -4,6 +4,9 @@ defenum(DataCruncherComponentEnum,
   "Elixir.AcqdatCore.DataCruncher.Functions.TSMax": 0,
   "Elixir.AcqdatCore.DataCruncher.Functions.TSFivepointSummary": 1,
   "Elixir.AcqdatCore.DataCruncher.Functions.TSMin": 2,
-  "Elixir.AcqdatCore.DataCruncher.Functions.Print": 3,
-  "Elixir.AcqdatCore.DataCruncher.Functions.Email": 4
+  "Elixir.AcqdatCore.DataCruncher.Functions.TSMean": 3,
+  "Elixir.AcqdatCore.DataCruncher.Functions.TSVariance": 4,
+  "Elixir.AcqdatCore.DataCruncher.Functions.TSStandardDeviation": 5,
+  "Elixir.AcqdatCore.DataCruncher.Functions.Print": 6,
+  "Elixir.AcqdatCore.DataCruncher.Functions.Email": 7
 )
