378ad9f66298a1fd7aa4d4ea86ceeaf7d0355f68
BandanaPandey <bandanapandey11@gmail.com>
2020-08-25 14:11:18 +0530

    added schema for dashboard's panel table

---
+++ apps/acqdat_core/lib/acqdat_core/dashboard_management/schema/panel.ex
@@ -0,0 +1,83 @@
+defmodule AcqdatCore.DashboardManagement.Schema.Panel do
+  @moduledoc """
+  Models a Dashboard Entity.
+
+  Dashboard are used for visually representing collection of widgets to user.
+
+  Any particular org can have multiple dashboards.
+
+  A dashboard consists of multiple widgets.
+  """
+  use AcqdatCore.Schema
+  alias AcqdatCore.Schema.EntityManagement.Organisation
+  alias AcqdatCore.DashboardManagement.Schema.{Dashboard, WidgetInstance, CommandWidget}
+
+  @typedoc """
+  `name`: Name of the dashboard, which will be unique with respective to org.
+  `uuid`: A universally unique id to identify the Dashboard.
+  `settings`: All the settings of dashboard
+  """
+  @type t :: %__MODULE__{}
+  schema("acqdat_panel") do
+    field(:name, :string, null: false)
+    field(:description, :string)
+    field(:uuid, :string, null: false)
+    field(:slug, :string, null: false)
+    field(:settings, :map)
+    field(:widget_layouts, :map)
+
+    # associations
+    belongs_to(:org, Organisation, on_replace: :delete)
+    belongs_to(:dashboard, Dashboard, on_replace: :delete)
+    has_many(:widget_instances, WidgetInstance, on_replace: :delete)
+    has_many(:command_widgets, CommandWidget)
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @required_params ~w(uuid slug name org_id dashboard_id)a
+  @optional_params ~w(settings description widget_layouts)a
+  @permitted @optional_params ++ @required_params
+
+  def changeset(%__MODULE__{} = dashboard, params) do
+    dashboard
+    |> cast(params, @permitted)
+    |> add_slug()
+    |> add_uuid()
+    |> validate_required(@required_params)
+    |> common_changeset()
+  end
+
+  def update_changeset(%__MODULE__{} = dashboard, params) do
+    dashboard
+    |> cast(params, @permitted)
+    |> validate_required(@required_params)
+    |> common_changeset()
+  end
+
+  @spec common_changeset(Ecto.Changeset.t()) :: Ecto.Changeset.t()
+  def common_changeset(changeset) do
+    changeset
+    |> assoc_constraint(:org)
+    |> assoc_constraint(:dashboard)
+    |> unique_constraint(:name,
+      name: :unique_panel_name_per_dashboard,
+      message: "unique panel name under dashboard"
+    )
+  end
+
+  # TODO: Need to remove these codes, after fbp branch is merged to master
+  defp add_uuid(%Ecto.Changeset{valid?: true} = changeset) do
+    changeset
+    |> put_change(:uuid, UUID.uuid1(:hex))
+  end
+
+  defp add_slug(%Ecto.Changeset{valid?: true} = changeset) do
+    changeset
+    |> put_change(:slug, Slugger.slugify(random_string(12)))
+  end
+
+  defp random_string(length) do
+    :crypto.strong_rand_bytes(length) |> Base.url_encode64() |> binary_part(0, length)
+  end
+end
