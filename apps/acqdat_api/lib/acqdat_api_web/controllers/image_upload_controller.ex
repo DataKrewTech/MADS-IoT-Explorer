8d010fc3cc63bbe6a5ecf6f4a70141bd624eb5e2
Chandra Shekhar <shekharait254@gmail.com>
2020-10-08 13:22:40 +0530

    mix format

--- apps/acqdat_api/lib/acqdat_api_web/controllers/image_upload_controller.ex
+++ apps/acqdat_api/lib/acqdat_api_web/controllers/image_upload_controller.ex
@@ -6,9 +6,10 @@ defmodule AcqdatApiWeb.ImageUploadController do
   def create(conn, %{"image" => image}) do
     with {:ok, image_name} <- Image.store({image, "dashboard"}) do
       image_url = Image.url({image_name, "dashboard"})
+
       conn
-          |> put_status(200)
-          |> render("show.json", %{url: image_url})
+      |> put_status(200)
+      |> render("show.json", %{url: image_url})
     else
       {:error, error} -> send_error(conn, 400, error)
     end
--- apps/acqdat_api/test/acqdat_api_web/controllers/image_upload_controller_test.exs
+++ apps/acqdat_api/test/acqdat_api_web/controllers/image_upload_controller_test.exs
@@ -8,16 +8,16 @@ defmodule AcqdatApiWeb.ImageUploadControllerTest do
     setup :setup_conn
 
     test "Uploading test", %{conn: conn} do
-
       image = %Plug.Upload{path: "test/assets/test_image.jpg", filename: "test_image.jpg"}
       params = %{"image" => image}
-      image_url = "https://datakrew-image.s3.ap-south-1.amazonaws.com/uploads/dashboard/test_image.jpg"
+
+      image_url =
+        "https://datakrew-image.s3.ap-south-1.amazonaws.com/uploads/dashboard/test_image.jpg"
 
       conn = post(conn, Routes.image_upload_path(conn, :create), params)
       response = conn |> json_response(200)
 
       assert response["url"] === image_url
-
     end
   end
 end
