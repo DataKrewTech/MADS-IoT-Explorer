51332121b148e7ea9a9110ca007af417797a4fc2
Ayoush <ayoushchourasia1418@gmail.com>
2020-10-23 11:47:43 +0530

    Changes in notification test

--- apps/notifications/test/notifications_test.exs
+++ apps/notifications/test/notifications_test.exs
@@ -2,7 +2,7 @@ defmodule NotificationsTest do
   use ExUnit.Case
   doctest Notifications
 
-  test "greets the world" do
-    assert Notifications.hello() == :world
-  end
+  # test "greets the world" do
+  #   assert Notifications.hello() == :world
+  # end
 end
