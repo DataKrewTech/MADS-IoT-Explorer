35d7a6a71325ece51dd18ea7ef7ead3ce87cb1c3
Ayoush <ayoushchourasia1418@gmail.com>
2020-11-11 23:55:30 +0530

    Added search tests for widget Added index tests for widget

--- apps/acqdat_api/test/acqdat_api_web/controllers/elasticsearch/widget_controller_test.exs
+++ apps/acqdat_api/test/acqdat_api_web/controllers/elasticsearch/widget_controller_test.exs
@@ -1,1 +1,120 @@
+defmodule AcqdatApiWeb.ElasticSearch.WidgetControllerTest do
+  use ExUnit.Case, async: true
+  use AcqdatApiWeb.ConnCase
+  use AcqdatCore.DataCase
+  alias AcqdatCore.Factory.ElasticSearch.Widget
+  import AcqdatCore.Support.Factory
 
+  describe "search_widgets/2" do
+    setup :setup_conn
+
+    test "fails if authorization header not found", %{conn: conn} do
+      widget = insert(:widget)
+      [widget: widget] = Widget.seed_widget(widget)
+      :timer.sleep(1500)
+      bad_access_token = "avcbd123489u"
+
+      conn =
+        conn
+        |> put_req_header("authorization", "Bearer #{bad_access_token}")
+
+      conn =
+        get(conn, Routes.widget_path(conn, :search_widget), %{
+          "label" => widget.label
+        })
+
+      result = conn |> json_response(403)
+      Widget.delete_index()
+      assert result == %{"errors" => %{"message" => "Unauthorized"}}
+    end
+
+    test "search with valid params", %{conn: conn} do
+      widget = insert(:widget)
+      [widget: widget] = Widget.seed_widget(widget)
+      :timer.sleep(1500)
+
+      conn =
+        get(conn, Routes.widget_path(conn, :search_widget), %{
+          "label" => widget.label
+        })
+
+      result = conn |> json_response(200)
+
+      Widget.delete_index()
+
+      assert result == %{
+               "widgets" => [
+                 %{
+                   "category" => widget.category,
+                   "id" => widget.id,
+                   "label" => widget.label,
+                   "properties" => widget.properties,
+                   "uuid" => widget.uuid
+                 }
+               ]
+             }
+    end
+
+    test "search with no hits", %{conn: conn, user: user} do
+      widget = insert(:widget)
+      [widget: widget] = Widget.seed_widget(widget)
+      :timer.sleep(1500)
+
+      conn =
+        get(conn, Routes.widget_path(conn, :search_widget), %{
+          "label" => user.first_name
+        })
+
+      result = conn |> json_response(200)
+      Widget.delete_index()
+
+      assert result == %{
+               "widgets" => []
+             }
+    end
+  end
+
+  describe "index widgets/2" do
+    setup :setup_conn
+
+    test "fails if authorization header not found", %{conn: conn} do
+      [widget1, widget2, widget3] = Widget.seed_multiple_widget()
+      :timer.sleep(1500)
+      bad_access_token = "avcbd123489u"
+
+      conn =
+        conn
+        |> put_req_header("authorization", "Bearer #{bad_access_token}")
+
+      conn =
+        get(conn, Routes.widget_path(conn, :index), %{
+          "from" => 0,
+          "page_size" => 1
+        })
+
+      result = conn |> json_response(403)
+      Widget.delete_index()
+      assert result == %{"errors" => %{"message" => "Unauthorized"}}
+    end
+
+    test "index with valid params and multiple entries", %{conn: conn} do
+      [widget1, widget2, widget3] = Widget.seed_multiple_widget()
+      :timer.sleep(1500)
+
+      conn =
+        get(conn, Routes.widget_path(conn, :index), %{
+          "from" => 0,
+          "page_size" => 3
+        })
+
+      %{"widgets" => widgets} = conn |> json_response(200)
+
+      Widget.delete_index()
+      assert length(widgets) == 3
+      [rwidget1, rwidget2, rwidget3] = widgets
+      assert rwidget1["id"] == widget1.id
+      assert rwidget2["id"] == widget2.id
+      assert rwidget3["id"] == widget3.id
+    end
+  end
+end
