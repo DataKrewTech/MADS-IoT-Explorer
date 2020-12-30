9c74a9e47457ca6871fa05e0b40849f2fa4f808a
Ayoush <ayoushchourasia1418@gmail.com>
2020-12-30 14:11:44 +0530

    Changes in user controller test for elastic search

--- apps/acqdat_api/test/acqdat_api_web/controllers/elasticsearch/user_controller_test.exs
+++ apps/acqdat_api/test/acqdat_api_web/controllers/elasticsearch/user_controller_test.exs
@@ -153,126 +153,81 @@ defmodule AcqdatApiWeb.ElasticSearch.UserControllerTest do
     end
   end
 
-  # describe "update and delete users/2" do
-  #   setup :setup_conn
+  describe "update and delete users/2" do
+    setup :setup_conn
 
-  #   test "if user is updated", %{conn: conn, user: user} do
-  #     User.create_index()
-  #     :timer.sleep(2500)
-  #     User.seed_user(user)
-  #     :timer.sleep(2500)
+    setup do
+      user = insert(:user)
+      User.create_index()
+      User.seed_user(user)
+      :timer.sleep(2500)
 
-  #     conn =
-  #       put(conn, Routes.user_path(conn, :update, user.org_id, user.id), %{
-  #         "first_name" => "Random User"
-  #       })
+      on_exit(fn ->
+        User.delete_index()
+      end)
 
-  #     :timer.sleep(5000)
+      [new_user: user]
+    end
 
-  #     conn =
-  #       get(conn, Routes.user_path(conn, :search_users, user.org_id), %{
-  #         "label" => "Random User"
-  #       })
+    test "if user is updated", %{conn: conn, new_user: user} do
+      conn =
+        put(conn, Routes.user_path(conn, :update, user.org_id, user.id), %{
+          "first_name" => "Random User"
+        })
 
-  #     result = conn |> json_response(200)
+      :timer.sleep(5000)
 
-  #     role = %{
-  #       "description" => user.role.description,
-  #       "id" => user.role.id,
-  #       "name" => user.role.name
-  #     }
+      conn =
+        get(conn, Routes.user_path(conn, :search_users, user.org_id), %{
+          "label" => "Random User"
+        })
 
-  #     organisation = %{"id" => user.org.id, "name" => user.org.name, "type" => "Organisation"}
-  #     User.delete_index()
+      result = conn |> json_response(200)
 
-  #     assert result == %{
-  #              "users" => [
-  #                %{
-  #                  "email" => user.email,
-  #                  "first_name" => "Random User",
-  #                  "id" => user.id,
-  #                  "last_name" => user.last_name,
-  #                  "role_id" => user.role_id,
-  #                  "org" => organisation,
-  #                  "role" => role,
-  #                  "image" => nil,
-  #                  "is_invited" => false,
-  #                  "phone_number" => nil,
-  #                  "user_setting" => nil
-  #                }
-  #              ],
-  #              "total_entries" => 1
-  #            }
-  #   end
+      role = %{
+        "description" => user.role.description,
+        "id" => user.role.id,
+        "name" => user.role.name
+      }
 
-  #   test "if user is deleted", %{conn: conn, user: user} do
-  #     User.create_index()
-  #     :timer.sleep(2500)
-  #     User.seed_user(user)
-  #     :timer.sleep(5000)
+      organisation = %{"id" => user.org.id, "name" => user.org.name, "type" => "Organisation"}
 
-  #     conn = delete(conn, Routes.user_path(conn, :delete, user.org_id, user.id))
+      assert result == %{
+               "users" => [
+                 %{
+                   "email" => user.email,
+                   "first_name" => "Random User",
+                   "id" => user.id,
+                   "last_name" => user.last_name,
+                   "role_id" => user.role_id,
+                   "org" => organisation,
+                   "role" => role,
+                   "image" => nil,
+                   "is_invited" => false,
+                   "phone_number" => nil,
+                   "user_setting" => nil
+                 }
+               ],
+               "total_entries" => 1
+             }
+    end
 
-  #     :timer.sleep(5000)
+    test "if user is deleted", %{conn: conn, new_user: user} do
+      conn = delete(conn, Routes.user_path(conn, :delete, user.org_id, user.id))
 
-  #     conn =
-  #       get(conn, Routes.user_path(conn, :search_users, user.org_id), %{
-  #         "label" => user.first_name
-  #       })
+      :timer.sleep(5000)
 
-  #     result = conn |> json_response(200)
-  #     User.delete_index()
+      conn =
+        get(conn, Routes.user_path(conn, :search_users, user.org_id), %{
+          "label" => user.first_name
+        })
 
-  #     assert result == %{
-  #              "users" => [],
-  #              "total_entries" => 0
-  #            }
-  #   end
+      result = conn |> json_response(200)
 
-  #   test "if user is deleted check for multiple users", %{conn: conn, org: org} do
-  #     User.create_index()
-  #     :timer.sleep(2500)
-  #     [user1, user2, _user3] = User.seed_multiple_user(org, 3)
-  #     :timer.sleep(2500)
-
-  #     conn = delete(conn, Routes.user_path(conn, :delete, user1.org_id, user1.id))
-
-  #     :timer.sleep(2500)
-
-  #     conn =
-  #       get(conn, Routes.user_path(conn, :search_users, user2.org_id), %{
-  #         "label" => user2.first_name
-  #       })
-
-  #     result = conn |> json_response(200)
-
-  #     role = %{
-  #       "description" => user2.role.description,
-  #       "id" => user2.role.id,
-  #       "name" => user2.role.name
-  #     }
-
-  #     organisation = %{"id" => user2.org.id, "name" => user2.org.name, "type" => "Organisation"}
-  #     User.delete_index()
-
-  #     assert result == %{
-  #              "users" => [
-  #                %{
-  #                  "email" => user2.email,
-  #                  "first_name" => user2.first_name,
-  #                  "id" => user2.id,
-  #                  "last_name" => user2.last_name,
-  #                  "role_id" => user2.role_id,
-  #                  "org" => organisation,
-  #                  "role" => role,
-  #                  "image" => nil,
-  #                  "is_invited" => false,
-  #                  "phone_number" => nil,
-  #                  "user_setting" => nil
-  #                }
-  #              ],
-  #              "total_entries" => 1
-  #            }
-  #   end
-  # end
+      assert result == %{
+               "users" => [],
+               "total_entries" => 0
+             }
+    end
+  end
 end
