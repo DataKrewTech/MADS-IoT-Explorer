7503a777772d0cf8fc93b42056054f96752d44cb
Ayoush <ayoushchourasia1418@gmail.com>
2020-12-30 12:08:41 +0530

    Changes in user seeding method

--- apps/acqdat_core/test/support/factory/elasticsearch/user.ex
+++ apps/acqdat_core/test/support/factory/elasticsearch/user.ex
@@ -1,6 +1,7 @@
 defmodule AcqdatCore.Factory.ElasticSearch.User do
   alias AcqdatApi.ElasticSearch
   import AcqdatCore.Support.Factory
+  alias AcqdatCore.Factory.ElasticSearch.User
   import Tirexs.HTTP
 
   def create_index() do
@@ -21,10 +22,14 @@ defmodule AcqdatCore.Factory.ElasticSearch.User do
   end
 
   def seed_multiple_user(org, count) do
+    User.create_index()
     [user1, user2, user3] = insert_list(count, :user, org: org)
     ElasticSearch.create_user("organisation", user1, user1.org)
+    :timer.sleep(2500)
     ElasticSearch.create_user("organisation", user2, user2.org)
+    :timer.sleep(2500)
     ElasticSearch.create_user("organisation", user3, user3.org)
+    :timer.sleep(2500)
     [user1, user2, user3]
   end
 end
