93e9d9ffc5bbfd1efe262b856ec724838c6b3be8
Ayoush <ayoushchourasia1418@gmail.com>
2021-01-15 12:34:25 +0530

    Added recent dashboard feature

--- apps/acqdat_api/lib/acqdat_api/helper/redis.ex
+++ apps/acqdat_api/lib/acqdat_api/helper/redis.ex
@@ -1,2 +1,60 @@
 defmodule AcqdatApi.Helper.Redis do
+  use Agent
+
+  def start_link(_opts) do
+    Agent.start_link(fn -> start_redis() end, name: __MODULE__)
+  end
+
+  defp start_redis() do
+    Redix.start_link(System.get_env("REDIS_PORT"), name: :redix)
+  end
+
+  def get_dashboard_ids(org_id) do
+    {:ok, pid} = Redix.start_link()
+
+    case Redix.command(pid, ["GET", org_id]) do
+      {:ok, nil} -> {:ok, []}
+      {:ok, ids} -> {:ok, ids |> to_charlist}
+      {:error, _} -> {:error, "Redis instance not running"}
+    end
+  end
+
+  def insert_dashboard(dashboard) do
+    {:ok, pid} = Redix.start_link()
+
+    case Redix.command(pid, ["GET", dashboard.org_id]) do
+      {:ok, nil} -> insert_org(dashboard.org_id, dashboard.id, pid)
+      {:ok, ids} -> update_org(dashboard.org_id, dashboard.id, pid, ids)
+      {:error, _} -> {:error, "Redis instance not running"}
+    end
+  end
+
+  defp insert_org(org_id, id, pid) do
+    Redix.command(pid, ["SET", org_id, [id]])
+  end
+
+  defp update_org(org_id, id, pid, ids) do
+    ids = ids |> to_charlist
+
+    ids =
+      case length(ids) do
+        5 ->
+          case Enum.member?(ids, id) do
+            true ->
+              ids
+
+            false ->
+              last_value = Enum.at(ids, 4)
+              [id] ++ (ids -- [last_value])
+          end
+
+        _ ->
+          case Enum.member?(ids, id) do
+            true -> ids
+            false -> [id] ++ ids
+          end
+      end
+
+    Redix.command(pid, ["SET", org_id, ids])
+  end
 end
--- apps/acqdat_api/lib/acqdat_api_web/controllers/dashboard_management/dashboard_controller.ex
+++ apps/acqdat_api/lib/acqdat_api_web/controllers/dashboard_management/dashboard_controller.ex
@@ -71,9 +71,16 @@ defmodule AcqdatApiWeb.DashboardManagement.DashboardController do
             send_error(conn, 400, message)
 
           {:ok, dashboard} ->
-            conn
-            |> put_status(200)
-            |> render("show.json", %{dashboard: dashboard})
+            case Redis.insert_dashboard(dashboard) do
+              {:ok, _} ->
+                conn
+                |> put_status(200)
+                |> render("show.json", %{dashboard: dashboard})
+
+              {:error, message} ->
+                conn
+                |> send_error(400, message)
+            end
         end
 
       404 ->
@@ -151,6 +158,33 @@ defmodule AcqdatApiWeb.DashboardManagement.DashboardController do
       401 ->
         conn
         |> send_error(401, "Unauthorized")
+    end
+  end
+
+  def recent_dashboard(conn, params) do
+    changeset = verify_index_params(params)
+
+    case conn.status do
+      nil ->
+        {:extract, {:ok, data}} = {:extract, extract_changeset_data(changeset)}
+
+        case Redis.get_dashboard_ids(data.org_id) do
+          {:ok, dashboard_ids} ->
+            data = Map.put_new(data, :dashboard_ids, dashboard_ids)
+            {:list, dashboards} = {:list, Dashboard.recent_dashboards(data)}
+
+            conn
+            |> put_status(200)
+            |> render("index.json", dashboards)
+
+          {:error, message} ->
+            conn
+            |> send_error(400, message)
+        end
+
+      404 ->
+        conn
+        |> send_error(404, "Resource Not Found")
     end
   end
 
