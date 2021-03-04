038d22bebc42343706f485598862f53511fa51cf
Ayoush <ayoushchourasia1418@gmail.com>
2021-03-04 18:00:07 +0530

    Completed the bug which arpit has reported in the user group update api which leads to policies not getting updated

--- apps/acqdat_api/lib/acqdat_api/api_access/user_group.ex
+++ apps/acqdat_api/lib/acqdat_api/api_access/user_group.ex
@@ -18,7 +18,7 @@ defmodule AcqdatApi.ApiAccess.UserGroup do
     verify_group(UserGroup.create(params))
   end
 
-  def update(group, %{actions: actions} = params) do
+  def update(group, %{"actions" => actions} = params) do
     params = for {key, val} <- params, into: %{}, do: {String.to_atom(key), val}
     policy_ids = Policy.extract_policies(actions)
     # present_policy_ids = UserGroup.policies(group.id)
