571260c0125c5df0ec4023c56c9da706a1d6d2ad
BandanaPandey <bandanapandey11@gmail.com>
2021-03-05 12:39:30 +0530

    Added support for legends in the query to generate data for line and area visualizations

--- apps/acqdat_core/lib/acqdat_core/data_insights/schema/visualizations/lines.ex
+++ apps/acqdat_core/lib/acqdat_core/data_insights/schema/visualizations/lines.ex
@@ -43,10 +43,24 @@ defmodule AcqdatCore.DataInsights.Schema.Visualizations.Lines do
 
       IO.inspect(output)
 
+      rows = output.rows
+
+      data =
+        if length(legends) > 0 and length(rows) > 0 do
+          [head | _] = output.columns
+
+          data =
+            rows |> Enum.group_by(fn [legend | _] -> legend end, fn [_legend | data] -> data end)
+
+          Enum.map(data, fn {key, value} -> %{name: "#{head} #{key}", data: value} end)
+        else
+          rows
+        end
+
       {:ok,
        %{
          headers: output.columns,
-         data: output.rows
+         data: data
        }}
     rescue
       error in Postgrex.Error ->
@@ -77,6 +91,45 @@ defmodule AcqdatCore.DataInsights.Schema.Visualizations.Lines do
   @impl true
   def data_settings() do
     Map.from_struct(__MODULE__).data_settings
+  end
+
+  defp compute_and_gen_data(fact_table_name, x_axes, y_axes, legends, filters)
+       when length(legends) > 0 do
+    [x_axis | _] = x_axes
+    x_axis_col = "\"#{x_axis["name"]}\""
+
+    [value | _] = y_axes
+    value_name = "\"#{value["name"]}\""
+
+    [legend | _] = legends
+    legend_name = "\"#{legend["name"]}\""
+
+    grouped_params = "\"#{legend["name"]}\"" <> "," <> x_axis_col
+
+    if x_axis["action"] == "group" do
+      values_data = y_axes_data(y_axes)
+
+      """
+        select #{legend_name},
+        EXTRACT(EPOCH FROM (time_bucket('#{x_axis["group_interval"]} #{x_axis["group_by"]}'::VARCHAR::INTERVAL,
+        to_timestamp("#{x_axis["name"]}", 'YYYY-MM-DD hh24:mi:ss'))))*1000 as \"#{x_axis["title"]}\",
+        #{values_data}
+        from #{fact_table_name}
+        where #{value_name} <> '' 
+        group by 1, 2
+        order by 1, 2
+      """
+    else
+      values_data = axes_data(y_axes, x_axis_col, legend_name)
+
+      """
+        select #{values_data}
+        from #{fact_table_name}
+        where #{value_name} <> '' 
+        group by #{grouped_params} 
+        order by #{grouped_params}
+      """
+    end
   end
 
   defp compute_and_gen_data(fact_table_name, x_axes, y_axes, legends, filters) do
@@ -133,6 +186,25 @@ defmodule AcqdatCore.DataInsights.Schema.Visualizations.Lines do
           }\""
       else
         x_axes <>
+          "," <> "#{value["action"]}(distinct(\"#{value["name"]}\")) as \"#{value["title"]}\""
+      end
+    end)
+  end
+
+  defp axes_data(y_axes, x_axes, legend) do
+    Enum.reduce(y_axes, x_axes, fn value, acc ->
+      if Enum.member?(["sum", "avg", "min", "max"], value["action"]) do
+        legend <>
+          "," <>
+          x_axes <>
+          "," <>
+          "ROUND(#{value["action"]}(CAST(\"#{value["name"]}\" AS NUMERIC)), 2) as \"#{
+            value["title"]
+          }\""
+      else
+        legend <>
+          "," <>
+          x_axes <>
           "," <> "#{value["action"]}(distinct(\"#{value["name"]}\")) as \"#{value["title"]}\""
       end
     end)
