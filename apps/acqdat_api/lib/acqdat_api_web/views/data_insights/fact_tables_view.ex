9134766072b6c5c6bf5c16fc07b50599dac61242
BandanaPandey <bandanapandey11@gmail.com>
2021-03-11 17:07:02 +0530

    Added fact_table headers view

--- apps/acqdat_api/lib/acqdat_api_web/views/data_insights/fact_tables_view.ex
+++ apps/acqdat_api/lib/acqdat_api_web/views/data_insights/fact_tables_view.ex
@@ -69,6 +69,10 @@ defmodule AcqdatApiWeb.DataInsights.FactTablesView do
     }
   end
 
+  def render("fact_table_headers.json", data) do
+    %{headers: data.headers}
+  end
+
   def render("index.json", fact_tables) do
     %{
       fact_tables: render_many(fact_tables.entries, FactTablesView, "fact_table.json"),
