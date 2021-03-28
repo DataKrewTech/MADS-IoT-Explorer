91b4455b1bbeaec56116385e868163b723222e7a
Ayoush <ayoushchourasia1418@gmail.com>
2021-03-28 12:44:51 +0530

    Handled error message for asset and sensor controller for two different scenarious

--- apps/acqdat_api/lib/acqdat_api_web/controllers/entity-management/sensor_controller.ex
+++ apps/acqdat_api/lib/acqdat_api_web/controllers/entity-management/sensor_controller.ex
@@ -88,7 +88,13 @@ defmodule AcqdatApiWeb.EntityManagement.SensorController do
             send_error(conn, 400, error)
 
           {:create, {:error, message}} ->
-            send_error(conn, 400, message)
+            response =
+              case is_map(message.error) do
+                false -> message
+                true -> message.error
+              end
+
+            send_error(conn, 400, response)
         end
 
       404 ->
