0c4e727b90eabfa3be8409846bccf9409117a5a2
arjun289 <arjun289singh@gmail.com>
2021-04-30 20:04:40 +0800

    Add unique constraint for the root assets

--- apps/acqdat_core/priv/repo/migrations/20210408065216_alter_sensor_make_sensor_name_unique_under_project.exs
+++ apps/acqdat_core/priv/repo/migrations/20210408065216_alter_sensor_make_sensor_name_unique_under_project.exs
@@ -5,11 +5,13 @@ defmodule AcqdatCore.Repo.Migrations.AlterSensorMakeSensorNameUniqueUnderProject
     drop unique_index("acqdat_asset", [:name, :parent_id, :org_id])
     create unique_index("acqdat_asset", [:name, :parent_id, :org_id, :project_id])
     create unique_index("acqdat_sensors", [:name, :parent_id, :project_id])
+    execute( "create unique index asset_root_unique_name on acqdat_asset (name, parent_id, project_id, coalesce(parent_id, -1));" )
   end
 
   def down do
     drop unique_index("acqdat_asset", [:name, :parent_id, :org_id, :project_id])
     drop unique_index("acqdat_sensors", [:name, :parent_id, :project_id])
+    execute( "drop unique index asset_root_unique_name on acqdat_asset (name, parent_id, project_id, coalesce(parent_id, -1));" )
     create unique_index("acqdat_asset", [:name, :parent_id, :org_id])
   end
 end
