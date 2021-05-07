8ed7b5ca598a7a819be0ae71e7586b8348252e13
BandanaPandey <bandanapandey11@gmail.com>
2021-05-07 14:12:32 +0530

    Added entities controller testcases

--- apps/acqdat_api/test/acqdat_api_web/controllers/entity-management/entity_controller_test.exs
+++ apps/acqdat_api/test/acqdat_api_web/controllers/entity-management/entity_controller_test.exs
@@ -21,11 +21,146 @@ defmodule AcqdatApiWeb.EntityManagement.EntityControllerTest do
 
       conn = get(conn, Routes.entity_path(conn, :fetch_hierarchy, 1, 1))
       result = conn |> json_response(403)
-      assert result == %{"errors" => %{"message" => "Unauthorized"}}
+
+      assert result == %{
+               "detail" => "You are not allowed to perform this action.",
+               "source" => nil,
+               "status_code" => 403,
+               "title" => "Unauthorized"
+             }
+    end
+
+    test "fails if invalid Organisation_id provided", %{conn: conn} do
+      conn = get(conn, Routes.entity_path(conn, :fetch_hierarchy, -1, 1))
+
+      result = conn |> json_response(404)
+
+      assert result == %{
+               "detail" => "Entity with this ID doesn't exists",
+               "source" => nil,
+               "status_code" => 404,
+               "title" => "Invalid entity ID"
+             }
     end
 
     test "shows hirerachy tree of the respective project", %{conn: conn, asset: asset} do
       conn = get(conn, Routes.entity_path(conn, :fetch_hierarchy, asset.org_id, asset.project_id))
+      result = conn |> json_response(200)
+
+      assert result["id"] == asset.org_id
+      assert result["type"] == "Organisation"
+    end
+  end
+
+  describe "update_hierarchy/2" do
+    setup :setup_conn
+
+    setup do
+      asset = insert(:asset)
+      [asset: asset]
+    end
+
+    test "fails if invalid token in authorization header", %{conn: conn} do
+      bad_access_token = "qwerty1234567qwerty12"
+
+      conn =
+        conn
+        |> put_req_header("authorization", "Bearer #{bad_access_token}")
+
+      conn = post(conn, Routes.entity_path(conn, :update_hierarchy, 1, 1), %{})
+      result = conn |> json_response(403)
+
+      assert result == %{
+               "detail" => "You are not allowed to perform this action.",
+               "source" => nil,
+               "status_code" => 403,
+               "title" => "Unauthorized"
+             }
+    end
+
+    test "fails if invalid Organisation_id provided", %{conn: conn} do
+      conn = post(conn, Routes.entity_path(conn, :update_hierarchy, -1, 1), %{})
+
+      result = conn |> json_response(404)
+
+      assert result == %{
+               "detail" => "Entity with this ID doesn't exists",
+               "source" => nil,
+               "status_code" => 404,
+               "title" => "Invalid entity ID"
+             }
+    end
+
+    test "successfully updates hirerachy tree", %{conn: conn, asset: asset} do
+      params = %{
+        org_id: asset.org_id,
+        name: "demo",
+        type: "Organisation",
+        entities: [
+          %{
+            id: asset.project,
+            type: "Project",
+            name: "Demo Proj",
+            entities: []
+          }
+        ]
+      }
+
+      conn =
+        post(
+          conn,
+          Routes.entity_path(conn, :update_hierarchy, asset.org_id, asset.project_id),
+          params
+        )
+
+      result = conn |> json_response(200)
+
+      assert result["id"] == asset.org_id
+      assert result["type"] == "Organisation"
+    end
+  end
+
+  describe "fetch_all_hierarchy/2" do
+    setup :setup_conn
+
+    setup do
+      asset = insert(:asset)
+      [asset: asset]
+    end
+
+    test "fails if invalid token in authorization header", %{conn: conn} do
+      bad_access_token = "qwerty1234567qwerty12"
+
+      conn =
+        conn
+        |> put_req_header("authorization", "Bearer #{bad_access_token}")
+
+      conn = get(conn, Routes.entity_path(conn, :fetch_all_hierarchy, 1))
+      result = conn |> json_response(403)
+
+      assert result == %{
+               "detail" => "You are not allowed to perform this action.",
+               "source" => nil,
+               "status_code" => 403,
+               "title" => "Unauthorized"
+             }
+    end
+
+    test "fails if invalid Organisation_id provided", %{conn: conn} do
+      conn = get(conn, Routes.entity_path(conn, :fetch_all_hierarchy, -1))
+
+      result = conn |> json_response(404)
+
+      assert result == %{
+               "detail" => "Entity with this ID doesn't exists",
+               "source" => nil,
+               "status_code" => 404,
+               "title" => "Invalid entity ID"
+             }
+    end
+
+    test "shows hirerachy tree of the respective project", %{conn: conn, asset: asset} do
+      conn = get(conn, Routes.entity_path(conn, :fetch_all_hierarchy, asset.org_id))
       result = conn |> json_response(200)
 
       assert result["id"] == asset.org_id
@@ -61,7 +196,13 @@ defmodule AcqdatApiWeb.EntityManagement.EntityControllerTest do
 
       conn = post(conn, Routes.entity_path(conn, :fetch_count), %{})
       result = conn |> json_response(403)
-      assert result == %{"errors" => %{"message" => "Unauthorized"}}
+
+      assert result == %{
+               "detail" => "You are not allowed to perform this action.",
+               "source" => nil,
+               "status_code" => 403,
+               "title" => "Unauthorized"
+             }
     end
 
     test "shows asset count per project", %{conn: conn, asset: asset} do
